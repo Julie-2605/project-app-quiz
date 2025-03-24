88126380f67a84a11d947abd7c9cb2f8b48fef22
MarianaLD14 <marianadias14b@hotmail.com>
2025-03-24 11:41:22 +0100

    Modification standalone

---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-quiz',
+  imports: [],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.component.css'
+})
+export class QuizComponent {
+
+}
