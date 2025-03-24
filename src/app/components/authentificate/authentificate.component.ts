cad0309b60a43958b7a0d99e42e2d288fdbaf662
MarianaLD14 <marianadias14b@hotmail.com>
2025-03-24 11:57:37 +0100

    Ajout components

---
+++ src/app/components/authentificate/authentificate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentificate',
+  imports: [],
+  templateUrl: './authentificate.component.html',
+  styleUrl: './authentificate.component.css'
+})
+export class AuthentificateComponent {
+
+}
