fc36761093f2d6088335e3722824dabf1ec84434
Julie-2605 <julie.cariou2605@gmail.com>
2025-03-27 09:39:37 +0100

    Ajout HeroComponent + AuthentificateComponent

--- src/app/components/authentificate/authentificate.component.ts
+++ src/app/components/authentificate/authentificate.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-authentificate',
   imports: [],
   templateUrl: './authentificate.component.html',
--- src/app/components/templates/hero/hero.component.spec.ts
+++ src/app/components/templates/hero/hero.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ApiComponent } from './api.component';
+import { HeroComponent } from './hero.component';
 
-describe('ApiComponent', () => {
-  let component: ApiComponent;
-  let fixture: ComponentFixture<ApiComponent>;
+describe('HeroComponent', () => {
+  let component: HeroComponent;
+  let fixture: ComponentFixture<HeroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApiComponent]
+      imports: [HeroComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ApiComponent);
+    fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
